782290048e0f49f3f6f920735dd48444b5511131
naseer ahmed <thenaseer.dev@gmail.com>
2021-06-16 03:20:40 +0500

    warning removed

---
+++ vue.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    //webpack configuration
+	configureWebpack: {
+	     //Turn off the performance tips of webpack
+	    performance: {
+		    hints:false
+	    },
+ 
+	     //Or
+ 
+	     //Warning webpack performance tips
+	    performance: {
+	    	hints:'warning',
+	    	 //Maximum volume at the beginning of the entrance
+	    	maxEntrypointSize: 50000000,
+	    	 //Maximum size of generated file
+	    	maxAssetSize: 30000000,
+	    	 //Only give performance tips for js files
+	    	assetFilter: function(assetFilename) {
+	    		return assetFilename.endsWith('.js');
+	    	}
+	    }
+    }
+}
