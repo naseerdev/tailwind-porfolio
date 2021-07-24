cba41a6dee3f53093e25b85cdf057f798ef6bc0f
naseer ahmed <thenaseer.dev@gmail.com>
2021-07-24 22:11:41 +0500

    links updated

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -19,7 +19,7 @@
             <br>
             <div class="flex flex-wrap justify-center mt-4">
                 <div v-for="item in socials" v-bind:key="item.name" class="flex-grow m-2 sm:mx-0 sm:my-4 sm:w-auto md:mx-2 bg-background-light rounded-sm">
-                    <a :href="item.link" class="p-4 text-center rounded-sm text-xl text-label-info hover:text-secondary-main hover:bg-transparent-secondary block">
+                    <a :href="item.link" target="_blank" class="p-4 text-center rounded-sm text-xl text-label-info hover:text-secondary-main hover:bg-transparent-secondary block">
                         <font-awesome-icon :icon="['fab', item.icon]" size="lg" /><br>
                         <span>{{item.name}}</span>
                     </a>
